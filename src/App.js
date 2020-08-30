0ffb1064f606443546c14bbadf87ce98d68042d6
nico <ndeleon@itba.edu.ar>
2020-08-30 02:10:20 -0300

    Fix index size forms links and logo metadata

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
               <div style={{"marginTop": "8px"}}>
                 <p className="home-l-text">Trabajá en casa</p>
                 <p className="home-l-text-stroke">de un vecino</p>
-                <p className="home-l-catchfrase">Espacios de co-working cercanos, flexibles y accesibles</p>
+                <p className="home-l-catchfrase">Espacios de co-working cercanos, flexibles y accesibles para estudiantes y profesionales</p>
               </div>
               <div style={{"marginTop": "2rem", "display": "flex"}}>
                 <div style={{"marginRight": "1rem"}}>
@@ -51,7 +51,7 @@ function App() {
             <p className="find-your-space-text">Encontrá un lugar cómodo y con buen WiFi para tener un dia productivo de trabajo con tu equipo u otros profesionales y estudiantes del barrio. Si tenés una empresa, reservá un espacio exclusivo para tus colaboradores sin compromiso de permanencia. Vos elegís el día, la zona y Ofizown te lo consigue.</p>
             <div className="find-space-button">
               <Button buttonStyle="btn--third" onClick={() => {
-                window.location.href = 'https://docs.google.com/forms/d/10C2qdrpiS3Sa_UOGh-Uj4OsSwJrmHhSNQt1EmixrfwA/prefill';
+                window.location.href = "https://forms.gle/CYTQFNMg8h62iYW39";
                 return null;
               }}><p style={{fontFamily:"core-sans-c-medium"}}>Encontrá tu espacio</p></Button>
             </div>
@@ -66,7 +66,7 @@ function App() {
             <p className="offer-your-space-text">Transformá un ambiente de tu casa en un espacio de trabajo y ofrecelo en Ofizown para generar ingresos. Vos determinas las reglas del lugar, estableces la disponibilidad y aprobás tus vicitas.</p>
             <div className="offer-space-button">
               <Button buttonStyle="btn--third" onClick={() => {
-                window.location.href = "https://docs.google.com/forms/d/1DbVzTrGAjct6ppACyB1tO_ybWou4Ze-Ys-Rgkr5iXOk/prefill";
+                window.location.href = "https://forms.gle/K9jfy31pUg5haqZC8";
                 return null;
               }}><p style={{"fontFamily":"core-sans-c-medium"}}>Ofrecé tu espacio</p></Button>
             </div>
